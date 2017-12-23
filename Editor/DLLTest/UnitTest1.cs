ea9c5a40ae4656d38fddae9125fd16cf2d3d4414
dastyk <dastykill@gmail.com>
2017-12-23 08:12:33 +0100

    First steps to calling c++ dll from c#

--- Editor/DLLTest/UnitTest1.cs
+++ Editor/DLLTest/UnitTest1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace DLLTest
 {
     [TestClass]
     public class UnitTest1
     {
+        [DllImport("ResourceHandler.dll")]
+        static extern UIntPtr CreateLoader(uint type);
+        [DllImport("ResourceHandler.dll")]
+        static extern Int32 InitLoader(UIntPtr loader, String filePath, int mode);
         [TestMethod]
         public void TestMethod1()
         {
+            UIntPtr bl;
+            bl = CreateLoader(0);
+            Assert.IsNotNull(bl, "Could not create binary loader");
+            long result = InitLoader(bl, "test.dat", 0);
+            Assert.IsTrue(result == 0, "Could not init Loader");
         }
     }
 }
