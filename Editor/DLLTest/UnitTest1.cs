9cdd7f384223b521fc926aaff8f1af2283d69945
dastyk <dastykill@gmail.com>
2017-12-24 20:48:16 +0100

    Added more wrapper functions for binary loader
    Added editor project

--- Editor/DLLTest/UnitTest1.cs
+++ Editor/DLLTest/UnitTest1.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-
+using System.Text;
+using System.IO;
 namespace DLLTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [DllImport("ResourceHandler.dll")]
-        static extern UIntPtr CreateLoader(uint type);
-        [DllImport("ResourceHandler.dll")]
-        static extern Int32 InitLoader(UIntPtr loader, String filePath, int mode);
+       
+
         [TestMethod]
         public void TestMethod1()
         {
-            UIntPtr bl;
-            bl = CreateLoader(0);
-            Assert.IsNotNull(bl, "Could not create binary loader");
-            long result = InitLoader(bl, "test.dat", 0);
-            Assert.IsTrue(result == 0, "Could not init Loader");
+            File.Delete("test.dat");
+            {
+                UIntPtr bl;
+                bl = CreateLoader(0);
+                Assert.IsNotNull(bl, "Could not create binary loader");
+                long result = InitLoader(bl, "test.dat", 0);
+                Assert.IsTrue(result == 0, "Could not init Loader");
+
+                string file = "Test File";
+                result = CreateS(bl, "TestFile", "Test", Encoding.ASCII.GetBytes(file), 9);
+                Assert.IsTrue(result == 0, "Could not create TestFile, Error: " + result.ToString());
+                string file2 = "Test File2";
+                result = CreateS(bl, "TestFile2", "Test", Encoding.ASCII.GetBytes(file2), 10);
+                Assert.IsTrue(result == 0, "Could not create TestFile2, Error: " + result.ToString());
+                Assert.IsTrue(GetNumberOfFiles(bl) == 2, "Not 2 files");
+                Assert.IsTrue(GetNumberOfTypes(bl) == 1, "Not 1 type");
+                Assert.IsTrue(GetTotalSizeOfAllFiles(bl) == 19, "Total size was not 19");
+            }
         }
     }
 }
---
+++ Editor/Editor/BinaryLoader_Wrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace Editor
+{
+    class BinaryLoader_Wrapper
+    {
+        [DllImport("ResourceHandler.dll")]
+        static extern UIntPtr CreateLoader(uint type);
+        [DllImport("ResourceHandler.dll")]
+        static extern Int32 InitLoader(UIntPtr loader, String filePath, int mode);
+        [DllImport("ResourceHandler.dll")]
+        static extern Int32 CreateS(UIntPtr loader, String guid, String type, [In, MarshalAs(UnmanagedType.LPArray)] byte[] rdata, UInt64 size);
+        [DllImport("ResourceHandler.dll")]
+        static extern Int32 GetNumberOfFiles(UIntPtr loader);
+        [DllImport("ResourceHandler.dll")]
+        static extern Int32 GetNumberOfTypes(UIntPtr loader);
+        [DllImport("ResourceHandler.dll")]
+        static extern Int64 GetTotalSizeOfAllFiles(UIntPtr loader);
+    }
+}
