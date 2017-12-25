d182d76b0010d28ff1ce8320e2f502e86c6c1d67
dastyk <dastykill@gmail.com>
2017-12-25 11:45:51 +0100

    Added GetFiles

--- Editor/Editor/BinaryLoader_Wrapper.cs
+++ Editor/Editor/BinaryLoader_Wrapper.cs
@@ -11,6 +11,14 @@ namespace Editor
         EDIT,
         READ
     }
+
+    public struct LoaderFile
+    {
+        public UInt32 guid { get; set; }
+        public UInt32 type { get; set; }
+        public String guid_str { get; set; }
+        public String type_str { get; set; }
+    }
     public class BinaryLoader_Wrapper
     {
         UIntPtr loader;
@@ -46,6 +54,18 @@ namespace Editor
         static extern Int32 GetSizeOfFileS_C(UIntPtr loader, String guid, String type, ref UInt64 size);
         [DllImport("ResourceHandler.dll")]
         static extern Int32 Defrag_C(UIntPtr loader);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct FILE_C
+        {
+            public UInt32 guid;
+            public UInt32 type;
+            public IntPtr guid_str;
+            public IntPtr type_str;
+        };
+        [DllImport("ResourceHandler.dll")]
+        static extern Int32 GetFiles_C(UIntPtr loader, out IntPtr files, out UInt32 numfiles);
+
         public BinaryLoader_Wrapper()
         {
             loader = CreateLoader(0);
@@ -109,6 +129,33 @@ namespace Editor
         public Int32 Defrag()
         {
             return Defrag_C(loader);
+        }
+        public Int32 GetFiles(ref List<LoaderFile> file_list)
+        {
+            IntPtr files = IntPtr.Zero;
+            UInt32 numFiles = 0;
+            Int32 result = GetFiles_C(loader, out files, out numFiles);
+            if(numFiles == 0)
+            {
+                return result;
+            }
+            
+            var dataEntrySize = Marshal.SizeOf(typeof(FILE_C));
+            var arrayValue = files;
+            for (var i = 0; i < numFiles; i++)
+            {
+                FILE_C cur = (FILE_C)Marshal.PtrToStructure(arrayValue, typeof(FILE_C));
+                LoaderFile lf = new LoaderFile { guid = cur.guid, type = cur.type };
+                lf.guid_str = Marshal.PtrToStringUni(cur.guid_str);
+                Marshal.FreeCoTaskMem(cur.guid_str);
+                lf.type_str = Marshal.PtrToStringUni(cur.type_str);
+                Marshal.FreeCoTaskMem(cur.type_str);
+                file_list.Add(lf);
+
+                arrayValue = new IntPtr(arrayValue.ToInt32() + dataEntrySize);
+            }
+            Marshal.FreeCoTaskMem(files);
+            return result;
         }
     }
 
