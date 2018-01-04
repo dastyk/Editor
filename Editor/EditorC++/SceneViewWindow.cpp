c35cf0fd1bd6852b5e6e9f4a6b38894864823653
dastyk <dastykill@gmail.com>
2018-01-04 09:38:27 +0100

    Added the sceneview

---
+++ Editor/EditorC++/SceneViewWindow.cpp
@@ -0,0 +1,2 @@
+#include "SceneViewWindow.h"
+
---
+++ Editor/EditorC++/SceneViewWindow.h
@@ -0,0 +1,100 @@
+#pragma once
+
+namespace EditorC {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for SceneViewWindow
+	/// </summary>
+	public ref class SceneViewWindow : public System::Windows::Forms::Form
+	{
+	public:
+		SceneViewWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~SceneViewWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::SplitContainer^  splitContainer1;
+	protected:
+	private: System::Windows::Forms::TreeView^  treeView1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->BeginInit();
+			this->splitContainer1->Panel1->SuspendLayout();
+			this->splitContainer1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer1->Location = System::Drawing::Point(0, 0);
+			this->splitContainer1->Name = L"splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this->splitContainer1->Panel1->Controls->Add(this->treeView1);
+			this->splitContainer1->Size = System::Drawing::Size(284, 261);
+			this->splitContainer1->SplitterDistance = 94;
+			this->splitContainer1->TabIndex = 0;
+			// 
+			// treeView1
+			// 
+			this->treeView1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->treeView1->Location = System::Drawing::Point(0, 0);
+			this->treeView1->Name = L"treeView1";
+			this->treeView1->Size = System::Drawing::Size(94, 261);
+			this->treeView1->TabIndex = 0;
+			// 
+			// SceneViewWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->splitContainer1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::SizableToolWindow;
+			this->Name = L"SceneViewWindow";
+			this->Text = L"SceneViewWindow";
+			this->splitContainer1->Panel1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
